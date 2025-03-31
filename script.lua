ebb82bd47b0cc4c6fc19e23b967f509f6623ab83
byte1z <107352784+byte1z@users.noreply.github.com>
2025-03-31 19:32:23 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:kick("Script gives you a ban pls wait till its going to be updated now i have to recode this script")
+game.Players.LocalPlayer:kick("Script gives you a ban pls wait till its going to be updated")
